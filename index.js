f91640b3dd522052956a79ccf673492bf903d2ce
mayowadavid <34623610+mayowadavid@users.noreply.github.com>
2022-10-20 21:46:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+const  parseString = require('xml2js').parseString;
+const { resolve } = require('path');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+app.use(express.static("public"));
+const url = 'https://www.sharkscope.com/api/roots/networks/PokerStars/activeTournaments?filter=Type:H,D,N,T,NL;Type!:SAT';
+
+
+app.get('/', (req, res) => {
+    const path = resolve('/index.html');
+    res.sendFile(path);
+});
+
+const headers = {'Username': 'jmarcel@live.com',
+                'Password': 'c6bfa8ffebaa4a28e8372b21b113ec10'
+            };
+
+app.get('/tournament', async (req, res) => {
+          fetch(url, {headers}).then(xml =>xml.text()).then(
+            (data)=> {
+              parseString(data, function (err, result) {
+                return res.json(result);
+            });
+            });
+})
+
+const port = 4000
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
